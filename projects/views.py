52576928585b419428389a23d6462387211841dc
sukanta-27 <sukanta.raj.27@gmail.com>
2021-08-11 02:05:12 +0530

    Add basic templates

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse, request
 
 # Create your views here.
+def projects(request):
+    return render(request, 'projects/projects.html')
+
+def project(request, pk):
+    return render(request, 'projects/single-project.html', {'pk':pk})
