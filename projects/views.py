25a4e2d2049827aac0501389e648db17d1d5e042
sukanta-27 <sukanta.raj.27@gmail.com>
2021-08-11 15:09:06 +0530

    Update templates with dynamic data

--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,13 @@
+from projects.models import Project
 from django.shortcuts import render
 from django.http import HttpResponse, request
+from .models import Project
 
 # Create your views here.
 def projects(request):
-    return render(request, 'projects/projects.html')
+    projectList = Project.objects.all()
+    return render(request, 'projects/projects.html', {'projects': projectList})
 
 def project(request, pk):
-    return render(request, 'projects/single-project.html', {'pk':pk})
+    projectObject = Project.objects.get(id=pk)
+    return render(request, 'projects/single-project.html', {'project':projectObject})
