0d3c59246f9accfbb87691f931a913ceabe52e65
sukanta-27 <sukanta.raj.27@gmail.com>
2021-08-11 13:26:29 +0530

    Add project, review and tag models

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+import uuid
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    demo_link = models.CharField(max_length=2000, null=True, blank=True)
+    source_link = models.CharField(max_length=2000, null=True, blank=True)
+    tags = models.ManyToManyField('Tag', blank=True)
+    votes_total = models.IntegerField(default=0, null=True, blank=True)
+    votes_ratio = models.IntegerField(default=0, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, \
+        primary_key=True, editable=False)
+
+    def __str__(self) -> str:
+        return self.title
+
+class Review(models.Model):
+    VOTE_TYPE = (
+        ('up', 'Up Vote'),
+        ('down', 'Down Vote')
+    )
+    # owner =
+    project = models.ForeignKey(to=Project, on_delete=CASCADE)
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=200, null=True, blank=True, choices=VOTE_TYPE)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, \
+        primary_key=True, editable=False)
+
+    def __str__(self) -> str:
+        return self.value
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, \
+        primary_key=True, editable=False)
+
+    def __str__(self) -> str:
+        return self.name
