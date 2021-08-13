fea2a7c0166542fb31895e7b06d3889449a8e6c2
sukanta-27 <sukanta.raj.27@gmail.com>
2021-08-14 03:06:19 +0530

    Add featured image upload feature and add image to single project page

--- projects/forms.py
+++ projects/forms.py
@@ -6,4 +6,5 @@ from .models import Project
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
-        fields = ['title', 'description', 'demo_link', 'source_link', 'tags']
+        fields = ['title', 'description', 'featured_image',\
+             'demo_link', 'source_link', 'tags']
