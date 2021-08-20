c737a3dc9ec93ad7034bb09f7eb8f194b0928930
sukanta-27 <sukanta.raj.27@gmail.com>
2021-08-20 21:47:36 +0530

    Add Profile model and owner field

--- projects/models.py
+++ projects/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.db.models.deletion import CASCADE
 import uuid
+from users.models import Profile
 
 # Create your models here.
 class Project(models.Model):
+    owner = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, blank=True)
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     featured_image = models.ImageField(null=True, blank=True, default='default.jpg')
