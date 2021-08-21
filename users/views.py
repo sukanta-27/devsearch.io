c7659b392a69f2aebe49bb7c8fa12ae5847d01e0
sukanta-27 <sukanta.raj.27@gmail.com>
2021-08-21 13:47:28 +0530

    Add profiles page

--- users/views.py
+++ users/views.py
@@ -1,7 +1,13 @@
 from django.http import request
 from django.shortcuts import render
+from .models import Profile
 
 # Create your views here.
 def profiles(request):
-    return render(request=request, template_name='users/profiles.html')
+    profileList = Profile.objects.all()
+    context = {
+        'profiles': profileList,
+    }
+    return render(request=request, \
+        template_name='users/profiles.html', context=context)
 
