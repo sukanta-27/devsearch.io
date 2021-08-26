98e962628f02c5bf6530be49bf87b757e47bff51
sukanta-27 <sukanta.raj.27@gmail.com>
2021-08-26 17:16:15 +0530

    Add user-profile page and view

--- users/urls.py
+++ users/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.profiles, name='profiles'),
+    path('profile/<str:pk>/', views.profile, name='user-profile')
 ]
--- users/views.py
+++ users/views.py
@@ -11,3 +11,13 @@ def profiles(request):
     return render(request=request, \
         template_name='users/profiles.html', context=context)
 
+def profile(request, pk):
+    profile = Profile.objects.get(id=pk)
+    topSkills = profile.skill_set.exclude(description__exact="")
+    otherSkills = profile.skill_set.filter(description="")
+    context = {
+        'profile': profile,
+        'topSkills': topSkills,
+        'otherSkills': otherSkills,
+    }
+    return render(request, 'users/user-profile.html', context)
